6603ea034570a251380cbc0bd46b8c47ee65da61
Nick-Lemy <n.kayiranga@alustudent.com>
2026-06-09 15:21:00 +0200

    refactor: add actual Product model with additional properties

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,16 +4,28 @@ namespace ProductCatalogue.Api.Models;
 
 public class Product
 {
-    [Required]
     public int Id { get; set; }
-
-    [Required]
-    [MinLength(3)]
     public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ProductCode { get; set; } = string.Empty;
+    public string Brand { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string TargetMarket { get; set; } = string.Empty;
+    public string Season { get; set} = string.Empty;
 
-    [MinLength(30)]
-    public string Description { get; set; } = "";
+    public ProductStatus Status { get; set; }
+    public ProductReadiness Readiness { get; set; }
 
-    public decimal Price { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
 
+
+    public enum ProductStatus
+    {
+        DRAFT, IN_REVIEW, PUBLISHED, ARCHIVED,
+    }
+    public enum ProductReadiness
+    {
+        READY, NOT_READY
+    }
 }
