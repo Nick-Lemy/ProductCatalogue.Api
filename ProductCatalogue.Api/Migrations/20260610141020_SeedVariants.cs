154726be240bd0e06e27330fea4571dac7260713
Nick-Lemy <n.kayiranga@alustudent.com>
2026-06-10 16:28:47 +0200

    feat: add migration files for seeding product variants in the database

---
+++ ProductCatalogue.Api/Migrations/20260610141020_SeedVariants.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductCatalogue.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVariants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
