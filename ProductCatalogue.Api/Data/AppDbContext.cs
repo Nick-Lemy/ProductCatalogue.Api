945b56c566a2924170772eb7ec95e77f2d1cca88
Nick-Lemy <n.kayiranga@alustudent.com>
2026-06-11 10:08:43 +0200

    refactor: primary AppDbContext constructor by integrating options directly in the class definition

--- ProductCatalogue.Api/Data/AppDbContext.cs
+++ ProductCatalogue.Api/Data/AppDbContext.cs
@@ -3,10 +3,8 @@ using ProductCatalogue.Api.Models;
 
 namespace ProductCatalogue.Api.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
     public DbSet<Product> Products { get; set; }
     public DbSet<Variant> Variants { get; set; }
 
