ff8f77fe5a41eab29c54f4c6146b3090b12fa4da
Nick-Lemy <n.kayiranga@alustudent.com>
2026-06-17 10:59:16 +0200

    fix: Correct access modifier for asset service in AssetsController

--- ProductCatalogue.Api/Controllers/AssetsController.cs
+++ ProductCatalogue.Api/Controllers/AssetsController.cs
@@ -11,7 +11,7 @@ public class AssetsController(
     IAssetService assetService
 ): ControllerBase
 {
-    IAssetService _assetService = assetService;
+    private readonly IAssetService _assetService = assetService;
 
     [HttpGet]
     [SwaggerOperation(
